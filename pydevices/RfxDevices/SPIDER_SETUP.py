9666016a3208aba68878aea1e86cb688e06fbf24
taliercio <cesare.taliercio@igi.cnr.it>
2018-03-30 14:26:46 +0200

    Feature: Added configuration parameters for ISEPS  EG and RF modulation

--- pydevices/RfxDevices/SPIDER_SETUP.py
+++ pydevices/RfxDevices/SPIDER_SETUP.py
@@ -1,3 +1,27 @@
+# 
+# Copyright (c) 2017, Massachusetts Institute of Technology All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 from MDSplus import mdsExceptions, Device
 
 class SPIDER_SETUP(Device):
@@ -50,5 +74,15 @@ class SPIDER_SETUP(Device):
      parts.append({'path':'.SPIDER:BON_STOP', 'type':'numeric', 'value':1})
      parts.append({'path':'.SPIDER:WREF_START', 'type':'numeric', 'value':0})
      parts.append({'path':'.SPIDER:WREF_STOP', 'type':'numeric', 'value':0})
+
+     parts.append({'path':'.ISEPS:EG_MOD_STATE', 'type':'text', 'value':'DISABLE'})
+     parts.append({'path':'.ISEPS:EG_MOD_FREQ', 'type':'numeric', 'value':1})
+     parts.append({'path':'.ISEPS:EG_MOD_DC', 'type':'numeric', 'value':50})
+
+     parts.append({'path':'.ISEPS:RF_MOD_STATE', 'type':'text', 'value':'DISABLE'})
+     parts.append({'path':'.ISEPS:RF_MOD_FREQ', 'type':'numeric','value':1})
+     parts.append({'path':'.ISEPS:RF_MOD_DC', 'type':'numeric','value':50})
+   
+     del(i)
 
 
